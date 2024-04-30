d6b608e71561edbd7fd01b7bf4fcc707bf6a33b2
Ippo03 <ippokratispantelidis@gmail.com>
2024-04-30 16:24:10 +0300

    added more filters + list of rooms

--- app/src/main/java/com/example/distributed_hotel_booking/UserHomeScreen.kt
+++ app/src/main/java/com/example/distributed_hotel_booking/UserHomeScreen.kt
@@ -2,7 +2,10 @@ package com.example.distributed_hotel_booking
 
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
+import android.graphics.BitmapFactory
 import android.widget.DatePicker
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -52,13 +55,28 @@ import java.util.*
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.OutlinedButton
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Slider
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+
 @SuppressLint("ResourceType")
 @Composable
-fun UserHomeScreen() {
+fun UserHomeScreen(navController: NavController) {
     val context = LocalContext.current
     val calendar = Calendar.getInstance()
 
@@ -88,6 +106,9 @@ fun UserHomeScreen() {
 
     val names = List(1000) { "Hello, World!" }
 
+    val selectedRating = remember { mutableStateOf(0f) }
+    var selectedGuests by remember { mutableStateOf(1) }
+
     // Temp object to show that the UserHomeScreen is working
     Column(
         modifier = Modifier
@@ -99,10 +120,12 @@ fun UserHomeScreen() {
         val searchQuery = remember { mutableStateOf("") }
 
         Text(
-            text = "Welcome to Hotel Booking!",
+            text = "Welcome to Hotel Booking Website!",
             style = TextStyle(
+                fontFamily = FontFamily.Serif,
                 fontWeight = FontWeight.Bold,
-                fontSize = 18.sp
+                fontSize = 24.sp,
+                letterSpacing = 1.5.sp,
             ),
             modifier = Modifier.padding(bottom = 8.dp)
         )
@@ -170,39 +193,92 @@ fun UserHomeScreen() {
             }
         }
 
-        // Dropdown for area
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                Text("Select the area")
-                Spacer(modifier = Modifier.size(8.dp))
-                // Dropdown for area
+            // Area selection
+            Column {
+                Text("Area")
+                // Dropdown menu for selecting the area
             }
 
-        }
-        // rating bar
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            Text("Select the minimum rating")
-            Spacer(modifier = Modifier.size(8.dp))
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text("Guests")
+                Column {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = selectedGuests == 1,
+                            onClick = { selectedGuests = 1 }
+                        )
+                        Text("1")
+
+                        RadioButton(
+                            selected = selectedGuests == 2,
+                            onClick = { selectedGuests = 2 }
+                        )
+                        Text("2")
+                    }
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = selectedGuests == 3,
+                            onClick = { selectedGuests = 3 }
+                        )
+                        Text("3")
+
+                        RadioButton(
+                            selected = selectedGuests == 4,
+                            onClick = { selectedGuests = 4 }
+                        )
+                        Text("4")
+                    }
+                }
+            }
+
             // Rating bar
-        }
-        // Number of guests
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-        ) {
-            Text("Select the number of guests")
-            Spacer(modifier = Modifier.size(8.dp))
-            // Radio buttons for selecting the number of guests
+            Column(
 
-        }
+            ) {
+                Text(
+                    "Current Rating: ${selectedRating.value.toInt()}",
+                    textAlign = TextAlign.Center, // Align text at the center horizontally
+                )
 
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "0", // Minimum value
+                        fontSize = 12.sp,
+                        modifier = Modifier.width(20.dp) // Adjust width to fit the text
+                    )
+
+                    Slider(
+                        value = selectedRating.value,
+                        onValueChange = { newValue -> selectedRating.value = newValue },
+                        valueRange = 0f..5f,
+                        steps = 0,
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                            .width(100.dp)
+                    )
+
+                    Text(
+                        text = "5", // Maximum value
+                        fontSize = 12.sp,
+                        modifier = Modifier.width(20.dp) // Adjust width to fit the text
+                    )
+                }
+            }
+        }
 
         // Button for search
         Button(
@@ -217,7 +293,7 @@ fun UserHomeScreen() {
             verticalArrangement = Arrangement.Center,
         ) {
             Text(text = "List of hotels will be displayed here")
-            Greetings()
+            RoomsList(navController)
         }
     }
 }
@@ -225,58 +301,69 @@ fun UserHomeScreen() {
 @Preview
 @Composable
 fun UserHomeScreenPreview() {
-    UserHomeScreen()
+    UserHomeScreen(navController = NavController(LocalContext.current))
 }
 
 @Composable
-private fun Greetings(
+private fun RoomsList(
+    navController: NavController,
     modifier: Modifier = Modifier,
-    names: List<String> = List(1000) { "$it" }
+    names: List<String> = List(8) { "$it" }
 ) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
-            Text(text = name)
+            RoomListItem(
+                onItemClick = {
+                    // Navigate to the room details screen when an item is clicked
+                    navController.navigate(Screen.RoomDetailsScreen.route)
+                }
+            )
         }
     }
 }
 
-//@Composable
-//fun AreaSelectionScreen() {
-//    Column(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .padding(16.dp),
-//        verticalArrangement = Arrangement.Center,
-//        horizontalAlignment = Alignment.CenterHorizontally
-//    ) {
-//        var expanded by remember { mutableStateOf(false) }
-//        var selectedArea by remember { mutableStateOf("") }
-//
-//        val areas = listOf("Area 1", "Area 2", "Area 3")
-//
-//        OutlinedButton(
-//            onClick = { expanded = true },
-//            modifier = Modifier.fillMaxWidth()
-//        ) {
-//            Text(text = if (selectedArea.isBlank()) stringResource(id = R.string.select_area) else selectedArea)
-//        }
-//
-//        DropdownMenu(
-//            expanded = expanded,
-//            onDismissRequest = { expanded = false }
-//        ) {
-//            areas.forEach { area ->
-//                DropdownMenuItem(onClick = {
-//                    selectedArea = area
-//                    expanded = false
-//                }) {
-//                    Text(text = area)
-//                }
-//            }
-//        }
-//    }
-//}
+@Composable
+fun RoomListItem(
+//    room: Room,
+    onItemClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onItemClick)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Hotel photo
+        // Hotel photo
+        Image(
+            painter = painterResource(id = R.drawable.booking_logo), // Replace R.drawable.hotel_photo with your actual image resource
+            contentDescription = "Hotel photo",
+            modifier = Modifier.size(64.dp)
+        )
 
+        Spacer(modifier = Modifier.width(16.dp))
+
+        // Title, rating, and number of people
+        Column {
+            Text(text = "Room Title", fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(4.dp))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(Icons.Default.Star, contentDescription = "Rating")
+                Text(text = "5", modifier = Modifier.padding(start = 4.dp))
+                Spacer(modifier = Modifier.width(8.dp))
+                Icon(Icons.Default.Person, contentDescription = "Number of people")
+                Text(text = "3", modifier = Modifier.padding(start = 4.dp))
+            }
+        }
+
+        // View button
+        Spacer(modifier = Modifier.weight(1f))
+        Button(onClick = onItemClick) {
+            Text(text = "View")
+        }
+    }
+}
 
 
 /*
